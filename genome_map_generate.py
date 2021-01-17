9be2cb2fac7c47ddb10effb06ca5f0db7d78819e
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-01-16 23:55:03 -0600

    Code is able to generate a simple diagram. Will add more detail

--- genome_map_generate.py
+++ genome_map_generate.py
@@ -3,13 +3,32 @@
 ##https://biopython.org/docs/1.75/api/Bio.GenBank.html   Read the genbank file
 ##http://biopython.org/DIST/docs/GenomeDiagram/userguide.pdf Make an image out of the genbank file
 #NumPy for math
-from Bio import GenBank
+#http://biopython.org/DIST/docs/tutorial/Tutorial.html
+#Installed ReportLab for graphics
 
-
+from reportlab.lib import colors
+from reportlab.lib.units import cm
+from Bio import GenBank, SeqIO, Graphics
+from Bio.Graphics import GenomeDiagram
 from numpy import *
 
-with open('Genome.gb') as file:
-    print(file.read())
-    assert not file.read()
-    file.seek(0)
-    assert file.read()
+
+gb_data = SeqIO.read("Genome.gb", "genbank")
+print(gb_data.seq)
+
+circle_diagram = GenomeDiagram.Diagram(gb_data.id)
+diagram_track = circle_diagram.new_track(1, name="Annotated Features")
+diagram_item_set = diagram_track.new_set()
+
+for item in gb_data.features:
+    if item.type != "gene":
+        continue
+    if len(diagram_item_set) % 2 == 0:
+        color = colors.black
+    else:
+        color = colors.green
+
+    diagram_item_set.add_feature(item, color=color, label=True)
+
+circle_diagram.draw(format="circular", circular=True, pagesize = (50 * cm, 50 * cm), start = 0, end = len(gb_data))
+circle_diagram.write("genome_diagram.png", "PNG")
