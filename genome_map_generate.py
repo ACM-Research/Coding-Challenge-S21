501a213436ea533e488dc84a56ee3fc8bde008e4
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-01-18 00:32:55 -0600

    Tried a new method on this branch. Wanted to make a second track with the coding regions but they appear to be the same.

--- genome_map_generate.py
+++ genome_map_generate.py
@@ -1,10 +1,10 @@
-#Biopython and GenomeDiagram for DNA visualization
+# Biopython and GenomeDiagram for DNA visualization
 ##https://biopython.org/wiki/Documentation General docs
 ##https://biopython.org/docs/1.75/api/Bio.GenBank.html   Read the genbank file
 ##http://biopython.org/DIST/docs/GenomeDiagram/userguide.pdf Make an image out of the genbank file
-#NumPy for math
-#http://biopython.org/DIST/docs/tutorial/Tutorial.html
-#Installed ReportLab for graphics
+# NumPy for math
+# http://biopython.org/DIST/docs/tutorial/Tutorial.html
+# Installed ReportLab for graphics
 
 from reportlab.lib import colors
 from reportlab.lib.units import cm
@@ -12,23 +12,42 @@ from Bio import GenBank, SeqIO, Graphics
 from Bio.Graphics import GenomeDiagram
 from numpy import *
 
+gb_data = SeqIO.read("Genome.gb", "genbank")  # Read Genome.gb and store it as a GenBank record
 
-gb_data = SeqIO.read("Genome.gb", "genbank")
-print(gb_data.seq)
 
-circle_diagram = GenomeDiagram.Diagram(gb_data.id)
-diagram_track = circle_diagram.new_track(1, name="Annotated Features")
-diagram_item_set = diagram_track.new_set()
+circle_diagram = GenomeDiagram.Diagram(gb_data.id)  # Create empty diagram
+gene_track = circle_diagram.new_track(0, name="Genes")  # Create empty track to display genes
+gene_set = gene_track.new_set()  # Set of features on track
+cds_track = circle_diagram.new_track(1, name="Coding Regions")
+cds_set = cds_track.new_set()
 
-for item in gb_data.features:
-    if item.type != "gene":
-        continue
-    if len(diagram_item_set) % 2 == 0:
-        color = colors.black
+gene_count = 1
+cds_count = 1
+
+
+for item in gb_data.features:  # Iterate through each item in the GenBank record object
+    print(item)
+    if item.type == "gene":
+        if gene_count < 0:
+            gene_set.add_feature(item, color=colors.blue, label=True, label_size=20, label_angle=0)
+        else:
+            gene_set.add_feature(item, color=colors.green, label=True, label_size=20, label_angle=0)
+        gene_count = gene_count * -1
+
+    elif item.type == "CDS":
+        if cds_count < 0:
+            cds_set.add_feature(item, color=colors.red, label=True, label_size=30, label_angle=90)
+        else:
+            cds_set.add_feature(item, color=colors.black, label=True, label_size=30, label_angle=90)
+        cds_count = cds_count * -1
+
     else:
-        color = colors.green
+        continue
 
-    diagram_item_set.add_feature(item, color=color, label=True)
 
-circle_diagram.draw(format="circular", circular=True, pagesize = (50 * cm, 50 * cm), start = 0, end = len(gb_data))
+
+
+
+
+circle_diagram.draw(format="circular", circular=True, pagesize=(50 * cm, 50 * cm), start=0, end=len(gb_data), circle_core=.25, track_size=.2)
 circle_diagram.write("genome_diagram.png", "PNG")
