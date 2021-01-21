9cfffd54e2b86dd36975893b8b0713c63a3685c2
Adem Odza <62858601+AdemOdza@users.noreply.github.com>
2021-01-20 21:39:07 -0600

    Removed CDS track as it was the same thing. Changed parameters slightly for better readability.

--- genome_map_generate.py
+++ genome_map_generate.py
@@ -12,42 +12,28 @@ from Bio import GenBank, SeqIO, Graphics
 from Bio.Graphics import GenomeDiagram
 from numpy import *
 
-gb_data = SeqIO.read("Genome.gb", "genbank")  # Read Genome.gb and store it as a GenBank record
+gb_data = SeqIO.read("Test.gb", "genbank")  # Read Genome.gb and store it as a GenBank record
 
 
 circle_diagram = GenomeDiagram.Diagram(gb_data.id)  # Create empty diagram
 gene_track = circle_diagram.new_track(0, name="Genes")  # Create empty track to display genes
 gene_set = gene_track.new_set()  # Set of features on track
-cds_track = circle_diagram.new_track(1, name="Coding Regions")
-cds_set = cds_track.new_set()
 
 gene_count = 1
-cds_count = 1
+
 
 
 for item in gb_data.features:  # Iterate through each item in the GenBank record object
     print(item)
     if item.type == "gene":
         if gene_count < 0:
-            gene_set.add_feature(item, color=colors.blue, label=True, label_size=20, label_angle=0)
+            gene_set.add_feature(item, color=colors.blue, label=True, label_size=1*cm, label_angle=0, sigil="ARROW", label_position='start', name_qualifiers = item.qualifiers)
         else:
-            gene_set.add_feature(item, color=colors.green, label=True, label_size=20, label_angle=0)
+            gene_set.add_feature(item, color=colors.green, label=True, label_size=1*cm, label_angle=0, sigil="ARROW", label_position='start')
         gene_count = gene_count * -1
-
-    elif item.type == "CDS":
-        if cds_count < 0:
-            cds_set.add_feature(item, color=colors.red, label=True, label_size=30, label_angle=90)
-        else:
-            cds_set.add_feature(item, color=colors.black, label=True, label_size=30, label_angle=90)
-        cds_count = cds_count * -1
 
     else:
         continue
-
-
-
-
-
 
 circle_diagram.draw(format="circular", circular=True, pagesize=(50 * cm, 50 * cm), start=0, end=len(gb_data), circle_core=.25, track_size=.2)
 circle_diagram.write("genome_diagram.png", "PNG")
